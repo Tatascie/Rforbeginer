83c25c285ed1fb1ba85cd7f73130b8064b308ff0
Tatascie <thao.huynh@tu-dortmund.de>
2021-05-18 15:17:43 +0200

    added example

---
+++ list_example.R
@@ -0,0 +1,23 @@
+
+
+myCourse <- list(Name="introduction to Lists", 
+                  Dates=as.Date(c("2016-09-08", "2017-11-13")),
+                  Tags=c("List", "R", "Fundamentals"),
+                  Ratings=c(9.7, 9.5, 9.8, 0.5, 9.3, 9.3, 8.9))
+
+lstCourse <- list(Name="Introduction to Lists", 
+                  Dates=as.Date(c("2016-09-28", "2017-11-13")),
+                  Tags=c("List", "R", "Fundamentals"),
+                  Ratings=c(9.7, 9.5, 9.8, 0.5, 9.3, 9.3, 8.9))
+
+#Mean rating without outlier :)
+#Other possibility to access list members
+#Obtain sublist of first two elements of list
+
+#Add element to list
+#Add element to sixth position
+#Delete element
+
+
+
+
